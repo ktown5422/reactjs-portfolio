4309fa49f3ac194c5a29e214b2026f403a9162aa
ktown5422 <ktown5422@gmail.com>
2020-12-06 18:35:51 -0600

    add resumedata to resume page and passed props through, also added styles

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -2,6 +2,14 @@ import React from 'react';
 import { Grid, Typography } from "@material-ui/core";
 import "./Resume.css";
 import resumeData from "../../utils/resumeData";
+import CustomTimeline, { CustomTimelineSeparator } from "../../components/Timeline/Timeline";
+import WorkIcon from "@material-ui/icons/Work";
+import TimelineItem from '@material-ui/lab/TimelineItem';
+import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
+import TimelineDot from '@material-ui/lab/TimelineDot';
+import TimelineConnector from '@material-ui/lab/TimelineConnector';
+import TimelineContent from '@material-ui/lab/TimelineContent';
+
 
 const Resume = () => {
     return (
@@ -17,8 +25,37 @@ const Resume = () => {
             </Grid>
         </Grid>
 
-        {/* Education and Experiences */}
-        <Grid container className="section"></Grid>
+        {/* Education and Experience */}
+        <Grid container className="section">
+            <Grid item className="section_title mb_30">
+                <span></span>
+                <h6 className="section_title_text ">Resume</h6>
+            </Grid>
+
+            <Grid item xs={12}>
+                <Grid container>
+                    {/* Experience */}
+                    <Grid item sm={12} md={6}> 
+                        <CustomTimeline title="Work Experience" icon={<WorkIcon />}>
+                            {resumeData.experience.map((experiences) => (
+                                <TimelineItem>
+                                    <CustomTimelineSeparator />
+                                    <TimelineContent>
+                                        <Typography classname="timeline_title">{experiences.title}</Typography>
+                                        <Typography variant="caption" classname="timeline_date">{experiences.date}</Typography>
+                                        <Typography variant="body2" classname="timeline_description">{experiences.description}</Typography>
+                                    </TimelineContent>
+                                </TimelineItem>
+                            ))}
+                        </CustomTimeline>
+                    </Grid>
+
+                    {/* Education */}
+                    <Grid item sm={12} md={6}></Grid>
+                </Grid>
+            </Grid>
+
+        </Grid>
 
         {/* Services */}
         <Grid container className="section"></Grid>
--- src/utils/resumeData.js
+++ src/utils/resumeData.js
@@ -24,5 +24,41 @@ export default {
       },
     },
 
-    about: "LaunchCode and Savvy Coder Graduate Looking to expand skills in Software Development as a Junior Software Engineer. I'm passionate about technology and working with creative problem solvers to build efficient and engaging web solutions."
-};
+    about: "LaunchCode and Savvy Coder Graduate Looking to expand skills in Software Development as a Junior Software Engineer. I'm passionate about technology and working with creative problem solvers to build efficient and engaging web solutions.",
+
+    experience: [
+      {
+        title: "Web Developer/Techniction - Cyber Bar",
+        date: "July 2020 - Present",
+        description: "We Offer iPhone Screen Replacement and Web Development Services"
+      },
+      {
+        title: "Freelance Web Developer - Upwork.com",
+        date: "December 2019 - Present",
+        description: "I specialize in building beautiful, responsive websites for small businesses. I provide useful and meaningful service to customers with their projects. I have the knowledge to complete the assigned task so that the job will be done. Self-managed and resourceful, able to manage a web development project from start to finish.",
+      },
+      {
+        title: "Ramp Agent - Southwest Airlines",
+        date: "April 2018 - Present",
+        description: "Handle all aspects of loading, stowing and unloading baggage, cargo airmail, air freight, ballast and company materials according to a predetermined plan received either electronically or manually from an Operations Agent.",
+      },
+    ],
+
+    education: [
+      {
+        title: "LaunchCode",
+        date: "April 2019 - December 2019",
+        description: "LC101 is a 20-week course designed to prepare participants to become web developers. The course is taught in several programming languages, including Python, Java, and Javascript. All students will complete the same core curriculum in Python and then learn a specialized language track designed to hone key skills that are in high demand.",
+      },
+      {
+        title: "Savvy Coders",
+        date: "January 2019 - April 2019",
+        description: "An accelerated 11-week coding course that includes the fundamentals of HTML5, CSS3, functional and object-oriented JavaScript. Training also covered advanced single web page implementation, interactions through the DOM, Web API’s, JSON and Node.js. Leadership and organizational skills were also refined with Agile fundamentals certification and training.",
+      },
+      {
+        title: "FreeCodeCamp",
+        date: "May 4 2015 - Present",
+        description: "Completed Online Courses to further my learning in HTML/CSS and Javascript - https://www.codecademy.com/profiles/ktown5422 ",
+      },
+    ]
+  };
