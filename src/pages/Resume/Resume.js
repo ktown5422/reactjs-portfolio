7dfc5f5c61049435f5e4b4f8dc061aaae48181d4
ktown5422 <ktown5422@gmail.com>
2020-12-11 12:48:36 -0600

    add grid container spacing and styles to services

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -30,7 +30,7 @@ const Resume = () => {
       </Grid>
 
       {/* Education and Experience */}
-      <Grid container className="section">
+      <Grid container className="section pb_45">
         <Grid item className="section_title mb_30">
           <span></span>
           <h6 className="section_title_text ">Resume</h6>
@@ -92,20 +92,20 @@ const Resume = () => {
       </Grid>
 
       {/* Services */}
-      <Grid container className="section">
+      <Grid container className="section pb_45">
         <Grid item className="section_title mb_30">
           <span></span>
           <h6 className="section_title_text ">My Services</h6>
         </Grid>
 
         <Grid item xs={12}>
-          <Grid container>
+          <Grid container spacing={3} justify="space-around">
             {resumeData.services.map(service => (
                 <Grid item xs={12} sm={6} md={3}>
                   <div className="service">
                     <Icon className="service_icon">{service.icon}</Icon>
-                    <Typography className="service_title">{service.title}</Typography>
-                    <Typography className="service_description">{service.description}</Typography>
+                    <Typography className="service_title" variant="h6">{service.title}</Typography>
+                    <Typography className="service_description" variant="body2">{service.description}</Typography>
                   </div>
                 </Grid>
             ))}
