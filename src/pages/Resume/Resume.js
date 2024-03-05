98a599ab4470d624c25a3a487634b3cda0027cfe
Kevin Townson <ktown5422@gmail.com>
2024-03-05 12:13:38 -0600

    add form functionality - KT

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -151,34 +151,36 @@ const Resume = () => {
 
       {/* Contact */}
       <Grid container spacing={6} className="section pt_45 pb_45">
-      {/* Contact Form */}
-        <Grid item xs={12} lg={7}>
-          <Grid container>
-            <Grid item className="section_title mb_30">
-              <span></span>
-              <h6 className="section_title_text">Contact Form</h6>
-            </Grid>
+        {/* Contact Form */}
+        <form name="contact" method="POST" data-netlify="true">
+          <Grid item xs={12} lg={7}>
+            <Grid container>
+              <Grid item className="section_title mb_30">
+                <span></span>
+                <h6 className="section_title_text">Contact Form</h6>
+              </Grid>
 
-            <Grid item xs={12}>
-              <Grid container spacing={3}>
-                <Grid item xs={12} sm={6}>
-                  <TextField fullWidth name="name" label="Name" />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField fullWidth name="email" label="E-mail" />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField fullWidth name="message" label="Message" multiline rows={4} />
-                </Grid>
-                <Grid item xs={12}>
-                  <CustomButton text="Submit" />
+              <Grid item xs={12}>
+                <Grid container spacing={3}>
+                  <Grid item xs={12} sm={6}>
+                    <TextField fullWidth name="name" label="Name" />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField fullWidth name="email" label="E-mail" />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField fullWidth name="message" label="Message" multiline rows={4} />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <CustomButton text="Submit" />
+                  </Grid>
                 </Grid>
               </Grid>
             </Grid>
           </Grid>
-        </Grid>
+        </form>
 
-      {/* Contact Information */}
+        {/* Contact Information */}
         <Grid item xs={12} lg={5}>
           <Grid container>
             <Grid item className="section_title mb_30">
@@ -190,22 +192,22 @@ const Resume = () => {
               <Grid container>
                 <Grid item xs={12}>
                   <Typography className="contactInfo_item">
-                     <span>Address: </span> {resumeData.address}
+                    <span>Address: </span> {resumeData.address}
                   </Typography>
                 </Grid>
                 <Grid item xs={12}>
                   <Typography className="contactInfo_item">
-                     <span>Phone: </span> {resumeData.phone}
+                    <span>Phone: </span> {resumeData.phone}
                   </Typography>
                 </Grid>
                 <Grid item xs={12}>
                   <Typography className="contactInfo_item">
-                     <span>Email: </span> {resumeData.email}
+                    <span>Email: </span> {resumeData.email}
                   </Typography>
                 </Grid>
               </Grid>
             </Grid>
-            
+
             <Grid item xs={12}>
               <Grid container className="contactInfo_socialsContainer">
                 {Object.keys(resumeData.socials).map((key) => (
@@ -218,7 +220,7 @@ const Resume = () => {
               </Grid>
             </Grid>
           </Grid>
-        </Grid>  
+        </Grid>
       </Grid>
     </>
   );
