73d609d72aaae83aa70b5fb81e77455c7a26f39c
Kevin Townson <ktown5422@gmail.com>
2024-03-05 12:52:11 -0600

    add form customization fix 2 - KT

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -164,8 +164,8 @@ const Resume = () => {
       </Grid>
 
       {/* Contact */}
-      <form name="contact" method="POST" data-netlify="true" onSubmit={handleSubmit}>
-        <Grid container spacing={6} className="section pt_45 pb_45">
+      <Grid container spacing={6} className="section pt_45 pb_45">
+        <form name="contact" method="POST" data-netlify="true" onSubmit={handleSubmit}>
           {/* Contact Form */}
           <Grid item xs={12} lg={7}>
             <Grid container>
@@ -193,50 +193,50 @@ const Resume = () => {
               </Grid>
             </Grid>
           </Grid>
+        </form>
 
-          {/* Contact Information */}
-          <Grid item xs={12} lg={5}>
-            <Grid container>
-              <Grid item className="section_title mb_30">
-                <span></span>
-                <h6 className="section_title_text">Contact Information</h6>
-              </Grid>
+        {/* Contact Information */}
+        <Grid item xs={12} lg={5}>
+          <Grid container>
+            <Grid item className="section_title mb_30">
+              <span></span>
+              <h6 className="section_title_text">Contact Information</h6>
+            </Grid>
 
-              <Grid item xs={12}>
-                <Grid container>
-                  <Grid item xs={12}>
-                    <Typography className="contactInfo_item">
-                      <span>Address: </span> {resumeData.address}
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Typography className="contactInfo_item">
-                      <span>Phone: </span> {resumeData.phone}
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Typography className="contactInfo_item">
-                      <span>Email: </span> {resumeData.email}
-                    </Typography>
-                  </Grid>
+            <Grid item xs={12}>
+              <Grid container>
+                <Grid item xs={12}>
+                  <Typography className="contactInfo_item">
+                    <span>Address: </span> {resumeData.address}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography className="contactInfo_item">
+                    <span>Phone: </span> {resumeData.phone}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography className="contactInfo_item">
+                    <span>Email: </span> {resumeData.email}
+                  </Typography>
                 </Grid>
               </Grid>
+            </Grid>
 
-              <Grid item xs={12}>
-                <Grid container className="contactInfo_socialsContainer">
-                  {Object.keys(resumeData.socials).map((key) => (
-                    <Grid item className="contactInfo_social">
-                      <a href={resumeData.socials[key].link}>
-                        {resumeData.socials[key].icon}
-                      </a>
-                    </Grid>
-                  ))}
-                </Grid>
+            <Grid item xs={12}>
+              <Grid container className="contactInfo_socialsContainer">
+                {Object.keys(resumeData.socials).map((key) => (
+                  <Grid item className="contactInfo_social">
+                    <a href={resumeData.socials[key].link}>
+                      {resumeData.socials[key].icon}
+                    </a>
+                  </Grid>
+                ))}
               </Grid>
             </Grid>
           </Grid>
         </Grid>
-      </form>
+      </Grid >
     </>
   );
 };
